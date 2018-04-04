e9824528b51fadacdecd58547603eb031c3ecf51
shearsy636 <36412666+shearsy636@users.noreply.github.com>
2018-04-04 15:13:58 -0400

    beginning of java, flex

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// var $img = $('img');
+
+// $img.click(function resiz(e){
+//   $img.css({
+//     height: '400%',
+//     width: '400%',
+//   });
+// });
+
+// $(".painting_grid").on("click", function(){
+//   $(this).css({
+//     "height" : "300%",
+//     "width" : "300%"}
+//     });
+
+  $(".painting_grid").on("click", function(){
+    $(this).css({"height" : "70%", "width" : "70%"});
+  });
+
+  // $(".painting_grid").on("click", function(){
+  //   $(this).animate( {
+  //     height : '80%', width : '80%',}
+  //     ,3000)
+  // });
+
+  
